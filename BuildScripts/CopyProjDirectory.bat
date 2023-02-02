efe7493f739b3a138b48254786a3b5f2fbafc553
Marlon Vermeulen <Marlon.Vermeulen@deltares.nl>
2023-02-02 10:34:23 +0100

    Update CopyProjBat with description regarding the copy

--- BuildScripts/CopyProjDirectory.bat
+++ BuildScripts/CopyProjDirectory.bat
@@ -1,5 +1,4 @@
-rem File "d:\checkouts\D-FAST\D-FAST_Bank_Erosion\dfastbe.dist\pyproj\datadir.py", line 109, in get_data_dir
-rem  pyproj.exceptions.DataDirError: Valid PROJ data directory not found. Either set the path using the environmental variable PROJ_LIB or with `pyproj.datadir.set_data_dir`.
+rem Load Proj files, referenced by Nuitka during its build and used by the tool during usage.
 cd ..
 mkdir dfastbe.dist\proj
 copy .venv\Lib\site-packages\pyproj\proj_dir\share\proj\* dfastbe.dist\proj
