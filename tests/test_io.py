f441b263fb1e1aa2889290c28ce85c9f838465e4
Mostafa Farrag <moah.farag@gmail.com>
2025-03-14 21:37:19 +0100

    refactor imports in the test_io.py file

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,9 +1,8 @@
-import context
-import dfastbe.io
+import dfastbe.io as df_io
 import configparser
 import os
-import numpy
-import netCDF4
+import numpy as np
+import netCDF4 as nc
 
 import pytest
 
@@ -28,7 +27,7 @@ class Test_load_program_texts():
         Testing load_program_texts.
         """
         print("current work directory: ", os.getcwd())
-        assert dfastbe.io.load_program_texts("tests/files/messages.UK.ini") == None
+        assert df_io.load_program_texts("tests/files/messages.UK.ini") == None
 
 class Test_log_text():
     def test_log_text_01(self):
@@ -37,7 +36,7 @@ class Test_log_text():
         """
         key = "confirm"
         with captured_output() as (out, err):
-            dfastbe.io.log_text(key)
+            df_io.log_text(key)
         outstr = out.getvalue().splitlines()
         strref = ['Confirm using "y" ...', '']
         assert outstr == strref
@@ -49,7 +48,7 @@ class Test_log_text():
         key = ""
         nr = 3
         with captured_output() as (out, err):
-            dfastbe.io.log_text(key, repeat=nr)
+            df_io.log_text(key, repeat=nr)
         outstr = out.getvalue().splitlines()
         strref = ['', '', '']
         assert outstr == strref
@@ -61,7 +60,7 @@ class Test_log_text():
         key = "reach"
         dict = {"reach": "ABC"}
         with captured_output() as (out, err):
-            dfastbe.io.log_text(key, dict=dict)
+            df_io.log_text(key, dict=dict)
         outstr = out.getvalue().splitlines()
         strref = ['The measure is located on reach ABC']
         assert outstr == strref
@@ -74,7 +73,7 @@ class Test_log_text():
         dict = {"reach": "ABC"}
         filename = "test.log"
         with open(filename, "w") as f:
-            dfastbe.io.log_text(key, dict=dict, file=f)
+            df_io.log_text(key, dict=dict, file=f)
         all_lines = open(filename, "r").read().splitlines()
         strref = ['The measure is located on reach ABC']
         assert all_lines == strref
@@ -84,26 +83,26 @@ class Test_get_filename():
         """
         Testing get_filename wrapper for get_text.
         """
-        assert dfastbe.io.get_filename("report.out") == "report.txt"
+        assert df_io.get_filename("report.out") == "report.txt"
 
 class Test_get_text():
     def test_get_text_01(self):
         """
         Testing get_text: key not found.
         """
-        assert dfastbe.io.get_text("@") == ["No message found for @"]
+        assert df_io.get_text("@") == ["No message found for @"]
 
     def test_get_text_02(self):
         """
         Testing get_text: empty line key.
         """
-        assert dfastbe.io.get_text("") == [""]
+        assert df_io.get_text("") == [""]
 
     def test_get_text_03(self):
         """
         Testing get_text: "confirm" key.
         """
-        assert dfastbe.io.get_text("confirm") == ['Confirm using "y" ...','']
+        assert df_io.get_text("confirm") == ['Confirm using "y" ...','']
 
 class Test_write_config():
     def test_write_config_01(self):
@@ -119,7 +118,7 @@ class Test_write_config():
         config["Group 2"]["K2"] = "2.0 0.2 0.02 0.0"
         config.add_section("Group 3")
         config["Group 3"]["LongKey"] = "3"
-        dfastbe.io.write_config(filename, config)
+        df_io.write_config(filename, config)
         all_lines = open(filename, "r").read().splitlines()
         all_lines_ref = ['[G 1]',
                          '  k 1     = V 1',
@@ -140,7 +139,7 @@ class Test_read_fm_map():
         filename = "tests/files/e02_f001_c011_simplechannel_map.nc"
         varname = "x"
         #location = "face"
-        datac = dfastbe.io.read_fm_map(filename, varname)
+        datac = df_io.read_fm_map(filename, varname)
         dataref = 41.24417604888325
         assert datac[1] == dataref
 
@@ -151,7 +150,7 @@ class Test_read_fm_map():
         filename = "tests/files/e02_f001_c011_simplechannel_map.nc"
         varname = "y"
         location = "edge"
-        datac = dfastbe.io.read_fm_map(filename, varname, location)
+        datac = df_io.read_fm_map(filename, varname, location)
         dataref = 7059.853000358055
         assert datac[1] == dataref
 
@@ -161,7 +160,7 @@ class Test_read_fm_map():
         """
         filename = "tests/files/e02_f001_c011_simplechannel_map.nc"
         varname = "face_node_connectivity"
-        datac = dfastbe.io.read_fm_map(filename, varname)
+        datac = df_io.read_fm_map(filename, varname)
         dataref = 2352
         assert datac[-1][1] == dataref
 
@@ -171,7 +170,7 @@ class Test_read_fm_map():
         """
         filename = "tests/files/e02_f001_c011_simplechannel_map.nc"
         varname = "sea_floor_depth_below_sea_surface"
-        datac = dfastbe.io.read_fm_map(filename, varname)
+        datac = df_io.read_fm_map(filename, varname)
         dataref = 3.894498393076889
         assert datac[1] == dataref
 
@@ -181,7 +180,7 @@ class Test_read_fm_map():
         """
         filename = "tests/files/e02_f001_c011_simplechannel_map.nc"
         varname = "Water level"
-        datac = dfastbe.io.read_fm_map(filename, varname)
+        datac = df_io.read_fm_map(filename, varname)
         dataref = 3.8871328177527262
         assert datac[1] == dataref
 
@@ -192,7 +191,7 @@ class Test_read_fm_map():
         filename = "tests/files/e02_f001_c011_simplechannel_map.nc"
         varname = "water level"
         with pytest.raises(Exception) as cm:
-            datac = dfastbe.io.read_fm_map(filename, varname)
+            datac = df_io.read_fm_map(filename, varname)
         assert str(cm.value) == 'Expected one variable for "water level", but obtained 0.'
         
 class Test_get_mesh_and_facedim_names():
@@ -201,7 +200,7 @@ class Test_get_mesh_and_facedim_names():
         Testing get_mesh_and_facedim_names.
         """
         filename = "tests/files/e02_f001_c011_simplechannel_map.nc"
-        name_and_dim = dfastbe.io.get_mesh_and_facedim_names(filename)
+        name_and_dim = df_io.get_mesh_and_facedim_names(filename)
         assert name_and_dim == ("mesh2d", "mesh2d_nFaces")
 
 class Test_copy_ugrid():
@@ -211,11 +210,11 @@ class Test_copy_ugrid():
         """
         src_filename = "tests/files/e02_f001_c011_simplechannel_map.nc"
         dst_filename = "test.nc"
-        meshname, facedim = dfastbe.io.get_mesh_and_facedim_names(src_filename)
-        dfastbe.io.copy_ugrid(src_filename, meshname, dst_filename)
+        meshname, facedim = df_io.get_mesh_and_facedim_names(src_filename)
+        df_io.copy_ugrid(src_filename, meshname, dst_filename)
         #
         varname = "face_node_connectivity"
-        datac = dfastbe.io.read_fm_map(dst_filename, varname)
+        datac = df_io.read_fm_map(dst_filename, varname)
         dataref = 2352
         assert datac[-1][1] == dataref
 
@@ -227,13 +226,13 @@ class Test_copy_var():
         """
         src_filename = "tests/files/e02_f001_c011_simplechannel_map.nc"
         dst_filename = "test.nc"
-        #
-        with netCDF4.Dataset(src_filename) as src:
-            with netCDF4.Dataset(dst_filename, "a") as dst:
-                dfastbe.io.copy_var(src, "mesh2d_s1", dst)
-        #
+
+        with nc.Dataset(src_filename) as src:
+            with nc.Dataset(dst_filename, "a") as dst:
+                df_io.copy_var(src, "mesh2d_s1", dst)
+
         varname = "sea_surface_height"
-        datac = dfastbe.io.read_fm_map(dst_filename, varname)
+        datac = df_io.read_fm_map(dst_filename, varname)
         dataref = 3.8871328177527262
         assert datac[1] == dataref
 
@@ -247,13 +246,13 @@ class Test_ugrid_add():
         facedim = "mesh2d_nFaces"
         #
         varname = "xxx"
-        ldata = numpy.zeros((4132))
+        ldata = np.zeros((4132))
         ldata[1] = 3.14159
         long_name = "added_variable"
         #
-        dfastbe.io.ugrid_add(dst_filename, varname, ldata, meshname, facedim, long_name)
+        df_io.ugrid_add(dst_filename, varname, ldata, meshname, facedim, long_name)
         #
-        datac = dfastbe.io.read_fm_map(dst_filename, long_name)
+        datac = df_io.read_fm_map(dst_filename, long_name)
         assert datac[1] == ldata[1]
 
 class Test_read_waqua_xyz():
@@ -262,11 +261,11 @@ class Test_read_waqua_xyz():
         Read WAQUA xyz file default column 2.
         """
         filename = "tests/files/read_waqua_xyz_test.xyc"
-        data = dfastbe.io.read_waqua_xyz(filename)
-        datar = numpy.array([3., 6., 9., 12.])
+        data = df_io.read_waqua_xyz(filename)
+        datar = np.array([3., 6., 9., 12.])
         print("data reference: ", datar)
         print("data read     : ", data)
-        assert numpy.shape(data) == (4,)
+        assert np.shape(data) == (4,)
         assert (data == datar).all() == True
 
     def test_read_waqua_xyz_02(self):
@@ -275,11 +274,11 @@ class Test_read_waqua_xyz():
         """
         filename = "tests/files/read_waqua_xyz_test.xyc"
         col = (1,2)
-        data = dfastbe.io.read_waqua_xyz(filename, col)
-        datar = numpy.array([[ 2., 3.], [ 5., 6.], [ 8., 9.], [11., 12.]])
+        data = df_io.read_waqua_xyz(filename, col)
+        datar = np.array([[ 2., 3.], [ 5., 6.], [ 8., 9.], [11., 12.]])
         print("data reference: ", datar)
         print("data read     : ", data)
-        assert numpy.shape(data) == (4,2)
+        assert np.shape(data) == (4,2)
         assert (data == datar).all() == True
 
 class Test_write_simona_box():
@@ -288,10 +287,10 @@ class Test_write_simona_box():
         Write small SIMONA BOX file.
         """
         filename = "test.box"
-        data = numpy.array([[1, 2, 3],[4, 5, 6],[7, 8, 9]])
+        data = np.array([[1, 2, 3],[4, 5, 6],[7, 8, 9]])
         firstm = 0
         firstn = 0
-        dfastbe.io.write_simona_box(filename, data, firstm, firstn)
+        df_io.write_simona_box(filename, data, firstm, firstn)
         all_lines = open(filename, "r").read().splitlines()
         all_lines_ref = ['      BOX MNMN=(   1,    1,    3,    3), VARIABLE_VAL=',
                          '          1.000       2.000       3.000',
@@ -304,10 +303,10 @@ class Test_write_simona_box():
         Write small SIMONA BOX file with offset.
         """
         filename = "test.box"
-        data = numpy.array([[0, 0, 0, 0, 0], [0, 0, 1, 2, 3],[0, 0, 4, 5, 6],[0, 0, 7, 8, 9]])
+        data = np.array([[0, 0, 0, 0, 0], [0, 0, 1, 2, 3],[0, 0, 4, 5, 6],[0, 0, 7, 8, 9]])
         firstm = 1
         firstn = 2
-        dfastbe.io.write_simona_box(filename, data, firstm, firstn)
+        df_io.write_simona_box(filename, data, firstm, firstn)
         all_lines = open(filename, "r").read().splitlines()
         all_lines_ref = ['      BOX MNMN=(   2,    3,    4,    5), VARIABLE_VAL=',
                          '          1.000       2.000       3.000',
@@ -320,10 +319,10 @@ class Test_write_simona_box():
         Write large SIMONA BOX file.
         """
         filename = "test.box"
-        data = numpy.zeros((15,15))
+        data = np.zeros((15,15))
         firstm = 0
         firstn = 0
-        dfastbe.io.write_simona_box(filename, data, firstm, firstn)
+        df_io.write_simona_box(filename, data, firstm, firstn)
         all_lines = open(filename, "r").read().splitlines()
         all_lines_ref = ['      BOX MNMN=(   1,    1,   15,   10), VARIABLE_VAL=']
         all_lines_ref.extend(['          0.000       0.000       0.000       0.000       0.000       0.000       0.000       0.000       0.000       0.000']*15)
@@ -340,7 +339,7 @@ class Test_absolute_path():
         rootdir = "d:" + os.sep + "some" + os.sep + "dir"
         afile = "d:" + os.sep + "some" + os.sep + "other" + os.sep + "dir" + os.sep + "file.ext"
         rfile = ".." + os.sep + "other" + os.sep + "dir" + os.sep + "file.ext"
-        assert dfastbe.io.absolute_path(rootdir, rfile) == afile
+        assert df_io.absolute_path(rootdir, rfile) == afile
 
     def test_absolute_path_02(self):
         """
@@ -348,7 +347,7 @@ class Test_absolute_path():
         """
         rootdir = "d:" + os.sep + "some" + os.sep + "dir"
         file = ""
-        assert dfastbe.io.absolute_path(rootdir, file) == file
+        assert df_io.absolute_path(rootdir, file) == file
 
     def test_absolute_path_03(self):
         """
@@ -356,7 +355,7 @@ class Test_absolute_path():
         """
         rootdir = "d:" + os.sep + "some" + os.sep + "dir"
         file = "e:" + os.sep + "some" + os.sep + "other" + os.sep + "dir" + os.sep + "file.ext"
-        assert dfastbe.io.absolute_path(rootdir, file) == file
+        assert df_io.absolute_path(rootdir, file) == file
 
     def test_absolute_path_04(self):
         """
@@ -365,7 +364,7 @@ class Test_absolute_path():
         rootdir = os.sep + "some" + os.sep + "dir"
         afile = os.sep + "some" + os.sep + "other" + os.sep + "dir" + os.sep + "file.ext"
         rfile = ".." + os.sep + "other" + os.sep + "dir" + os.sep + "file.ext"
-        assert dfastbe.io.absolute_path(rootdir, rfile) == afile
+        assert df_io.absolute_path(rootdir, rfile) == afile
 
 class Test_relative_path():
     def test_relative_path_01(self):
@@ -375,7 +374,7 @@ class Test_relative_path():
         rootdir = "d:" + os.sep + "some" + os.sep + "dir"
         afile = "d:" + os.sep + "some" + os.sep + "other" + os.sep + "dir" + os.sep + "file.ext"
         rfile = ".." + os.sep + "other" + os.sep + "dir" + os.sep + "file.ext"
-        assert dfastbe.io.relative_path(rootdir, afile) == rfile
+        assert df_io.relative_path(rootdir, afile) == rfile
 
     def test_relative_path_02(self):
         """
@@ -383,7 +382,7 @@ class Test_relative_path():
         """
         rootdir = "d:" + os.sep + "some" + os.sep + "dir"
         file = ""
-        assert dfastbe.io.relative_path(rootdir, file) == file
+        assert df_io.relative_path(rootdir, file) == file
 
     def test_relative_path_03(self):
         """
@@ -391,7 +390,7 @@ class Test_relative_path():
         """
         rootdir = "d:" + os.sep + "some" + os.sep + "dir"
         file = "e:" + os.sep + "some" + os.sep + "other" + os.sep + "dir" + os.sep + "file.ext"
-        assert dfastbe.io.relative_path(rootdir, file) == file
+        assert df_io.relative_path(rootdir, file) == file
 
     def test_relative_path_04(self):
         """
@@ -400,7 +399,7 @@ class Test_relative_path():
         rootdir = os.sep + "some" + os.sep + "dir"
         afile = os.sep + "some" + os.sep + "other" + os.sep + "dir" + os.sep + "file.ext"
         rfile = ".." + os.sep + "other" + os.sep + "dir" + os.sep + "file.ext"
-        assert dfastbe.io.relative_path(rootdir, afile) == rfile
+        assert df_io.relative_path(rootdir, afile) == rfile
 
 # TODO: read_xyc
 # TODO: write_km_eroded_volumes
