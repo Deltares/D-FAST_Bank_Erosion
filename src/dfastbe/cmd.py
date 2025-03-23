7d6626344db673ca6df75921a3304a9ff55cecd4
Mostafa Farrag <moah.farag@gmail.com>
2025-03-23 22:29:27 +0100

    reformat `cmd`

--- src/dfastbe/cmd.py
+++ src/dfastbe/cmd.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Copyright (C) 2020 Stichting Deltares.
 
@@ -26,52 +25,82 @@ Stichting Deltares. All rights reserved.
 INFORMATION
 This file is part of D-FAST Bank Erosion: https://github.com/Deltares/D-FAST_Bank_Erosion
 """
+
 import os
-from dfastbe.bank_lines import banklines
+
 from dfastbe.bank_erosion import bankerosion
+from dfastbe.bank_lines import banklines
 from dfastbe.gui import main
 from dfastbe.io import get_progloc, load_program_texts
 
 
 def run(
-    language: str = "UK", runmode: str = "GUI", configfile: str = "dfastbe.cfg",
+    language: str = "UK",
+    run_mode: str = "GUI",
+    configfile: str = "dfastbe.cfg",
 ) -> None:
     """
-    Main routine initializing the language file and starting the chosen run mode.
-    
-    Arguments
-    ---------
-    language: str
-        Display language 'NL' or 'UK' ('UK' is default)
-    runmode: str
-        Run mode 'BANKLINES', 'BANKEROSION' or 'GUI' ('GUI' is default)
-    configfile: str
-        Configuration file ('dfastbe.cfg' is default)
+    Initializes the language file and starts the chosen run mode.
+
+    This function loads the appropriate language file and executes one of the
+    available modes: 'BANKLINES', 'BANKEROSION', or 'GUI'. The default configuration
+    file is `dfastbe.cfg`.
+
+    Args:
+        language (str, optional):
+            Display language code. Acceptable values are 'NL' (Dutch) or 'UK' (English).
+            Defaults to 'UK'.
+        run_mode (str, optional):
+            Mode in which the program should run. Available options:
+
+            - 'BANKLINES': Runs the bank lines processing.
+            - 'BANKEROSION': Runs the bank erosion processing.
+            - 'GUI': Launches the graphical user interface.
+
+            Defaults to 'GUI'.
+        configfile (str, optional):
+            Path to the configuration file. Defaults to 'dfastbe.cfg'.
+
+    Returns:
+        None
+
+    Raises:
+        Exception: If an invalid `run_mode` is provided. The valid options are
+            'BANKLINES', 'BANKEROSION', or 'GUI'.
+
+    Example:
+        Running the program with Dutch language and bank erosion mode:
+
+        ```python
+        run(language="NL", run_mode="BANKEROSION", configfile="custom_config.cfg")
+        ```
+
+        Running the program in default mode (GUI) with English language:
+
+        ```python
+        run()
+        ```
     """
-    progloc = get_progloc()
-    LANGUAGE = language.upper()
+    prog_loc = get_progloc()
+    language = language.upper()
     try:
-        load_program_texts(
-            progloc + os.path.sep + "messages." + LANGUAGE + ".ini"
-        )
+        load_program_texts(f"{prog_loc}{os.path.sep}messages.{language}.ini")
     except:
-        if LANGUAGE == "NL":
-            print(
-                f"Het taalbestand 'messages.{LANGUAGE}.ini' kan niet worden geladen."
-            )
+        if language == "NL":
+            print(f"Het taalbestand 'messages.{language}.ini' kan niet worden geladen.")
         else:
-            print(f"Unable to load language file 'messages.{LANGUAGE}.ini'")
+            print(f"Unable to load language file 'messages.{language}.ini'")
     else:
-        RUNMODE = runmode.upper()
-        if RUNMODE == "BANKLINES":
+        run_mode = run_mode.upper()
+        if run_mode == "BANKLINES":
             banklines(configfile)
-        elif RUNMODE == "BANKEROSION":
+        elif run_mode == "BANKEROSION":
             bankerosion(configfile)
-        elif RUNMODE == "GUI":
+        elif run_mode == "GUI":
             main(configfile)
         else:
             raise Exception(
                 "Invalid run mode '{}' specified. Should read 'BANKLINES', 'BANKEROSION' or 'GUI'.".format(
-                    runmode
+                    run_mode
                 )
             )
