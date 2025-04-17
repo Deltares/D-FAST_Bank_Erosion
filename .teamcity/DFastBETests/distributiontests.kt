ab22877cdebd451b41ad4f478f3a9b6aeff22a92
Jeroen <jeroen.noort@deltares.nl>
2025-04-17 14:06:11 +0200

    Add distributiontests configuration

---
+++ .teamcity/DFastBETests/distributiontests.kt
@@ -0,0 +1,91 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.commitStatusPublisher
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.failureConditions.BuildFailureOnMetric
+import jetbrains.buildServer.configs.kotlin.failureConditions.failOnMetricChange
+import DFastCleanConfiguration
+
+object DistributionTests : BuildType({
+    templates(DFastCleanConfiguration)
+    id("DistributionTests")
+    name = "Distribution Tests"
+
+    buildNumberPattern = "${BuildWithCommandWindow.depParamRefs["build.revisions.short"]}"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            name = "Install dependencies via poetry"
+            id = "Install_dependencies_via_poetry"
+            scriptContent = """
+                CALL conda activate %CONDA_ENV_NAME%
+                CALL pip install --upgrade virtualenv
+                CALL python -m poetry install
+                CALL python -m poetry show
+                CALL conda deactivate
+            """.trimIndent()
+        }
+        script {
+            name = "Get folder listing"
+            id = "Get_folder_listing"
+            scriptContent = """
+                echo "Listing current folder"
+                dir
+                echo "------------------"
+            """.trimIndent()
+        }
+        script {
+            name = "Validate distribution"
+            id = "Validate_distribution"
+            scriptContent = """
+                rem echo on
+                rem CALL poetry env use %%python3913%%\python.exe
+                CALL conda activate %CONDA_ENV_NAME%
+                CALL poetry run pytest -v tests/test_binaries/ --no-cov
+                CALL conda deactivate
+            """.trimIndent()
+        }
+        stepsOrder = arrayListOf("Conda_create_environment", "Python_pip_install_poetry", "Install_dependencies_via_poetry", "Unit_test_and_code_coverage", "Get_folder_listing", "Validate_distribution", "Conda_deactivate_and_remove_environment")
+    }
+
+    failureConditions {
+        failOnMetricChange {
+            id = "BUILD_EXT_469"
+            metric = BuildFailureOnMetric.MetricType.TEST_COUNT
+            units = BuildFailureOnMetric.MetricUnit.DEFAULT_UNIT
+            comparison = BuildFailureOnMetric.MetricComparison.LESS
+            compareTo = value()
+        }
+    }
+
+    features {
+        commitStatusPublisher {
+            id = "BUILD_EXT_315"
+            vcsRootExtId = "${DslContext.settingsRoot.id}"
+            publisher = github {
+                githubUrl = "https://api.github.com"
+                authType = personalToken {
+                    token = "%github_teamcity_commit_status_token%"
+                }
+            }
+        }
+    }
+
+    dependencies {
+        dependency(DFast_DFastBankErosion_SignedRelease) {
+            snapshot {
+                onDependencyFailure = FailureAction.FAIL_TO_START
+            }
+
+            artifacts {
+                id = "ARTIFACT_DEPENDENCY_5392"
+                artifactRules = "*.zip!** => dfastbe.dist"
+            }
+        }
+    }
+    
+    disableSettings("Unit_test_and_code_coverage")
+})
