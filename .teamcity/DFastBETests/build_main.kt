d53896c4a6b23147fb13385e91096d4f94288845
Jeroen <143803944+jeroen-deltares@users.noreply.github.com>
2025-04-23 21:08:53 +0200

    feat(ci): restructure and move TeamCity configurations and templates to the github repo (#87)
    feat(ci): restructure and move TeamCity configurations and templates to the github repo
    
    - Create and configure unit tests, distribution tests, and various build types (e.g. signed releases, command window variants)
    - Introduce and refactor reusable templates (`condatemplate`)
    - Reduce redundancy by centralizing and cleaning build steps and parameters
    - Add CI documentation and configuration descriptions
    - Define build order and update dependency references
    - Improve naming conventions and remove unused code and IDs
    - Disable automatic triggering and manage triggers via context parameters
    - Set paths and environment settings as parameters for flexibility
    
    refs: dfast-290

---
+++ .teamcity/DFastBETests/build_main.kt
@@ -0,0 +1,57 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.failureConditions.BuildFailureOnText
+import jetbrains.buildServer.configs.kotlin.failureConditions.failOnText
+
+object BuildMain : BuildType({
+    templates(CondaTemplate)
+    name = "Build without command window"
+    description = "Build D-FAST Bank Erosion without terminal window as main distribution"
+
+    artifactRules = """
+        dfastbe.dist => dfastbe.zip
+        compilation-report.xml =>.
+    """.trimIndent()
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            name = "build D-FAST BE"
+            id = "build_D_FAST_BE"
+            scriptContent = """
+                CALL conda activate %CONDA_ENV_NAME%
+                CALL .\BuildScripts\BuildDfastbe_no_command_window.bat
+                CALL conda deactivate
+            """.trimIndent()
+        }
+        stepsOrder = arrayListOf("Conda_create_environment", "Python_pip_install_poetry", "Install_dependencies_via_poetry", "build_D_FAST_BE", "Conda_deactivate_and_remove_environment")
+    }
+
+    failureConditions {
+        failOnText {
+            conditionType = BuildFailureOnText.ConditionType.CONTAINS
+            pattern = "AssertionError"
+            failureMessage = "AssertionError"
+            reverse = false
+            stopBuildOnFailure = true
+        }
+    }
+
+    dependencies {
+        snapshot(TestBinaries) {
+            onDependencyFailure = FailureAction.FAIL_TO_START
+        }
+        dependency(LatexManual) {
+            snapshot {
+                onDependencyFailure = FailureAction.FAIL_TO_START
+            }
+
+            artifacts {
+                artifactRules = "+:*.pdf => docs/"
+            }
+        }
+    }
+})
