d5749730efc3d2aa89956eeeaed47230412af9df
Mostafa Farrag <moah.farag@gmail.com>
2025-05-02 00:53:33 +0200

    move debugging lines to separate functions

--- src/dfastbe/bank_erosion/bank_erosion.py
+++ src/dfastbe/bank_erosion/bank_erosion.py
@@ -313,9 +313,9 @@ class Erosion:
             zfw_ini.append(simulation_data.water_level_face[ii])
         fairway_data.fairway_initial_water_levels = zfw_ini
 
-    def debug_process_discharge_levels_2(
-            self, ib, bank_data, fairway_data, erosion_inputs, pars, hfw, dn_eq1, dv_eq1, bcrds, bank_height,
-            line_size
+    def debug_process_discharge_levels_1(
+        self, ib, bank_data, fairway_data, erosion_inputs, pars, hfw, dn_eq1, dv_eq1, bcrds, bank_height,
+        line_size
     ):
         bcrds_mid = (bcrds[:-1] + bcrds[1:]) / 2
         bank_coords_points = [Point(xy) for xy in bcrds_mid]
@@ -346,6 +346,47 @@ class Erosion:
 
         write_shp(bank_coords_geo, params, f"{str(self.river_data.output_dir)}{os.sep}debug.EQ.B{ib + 1}.shp")
         write_csv(params, f"{str(self.river_data.output_dir)}{os.sep}debug.EQ.B{ib + 1}.csv")
+
+    def debug_process_discharge_levels_2(
+        self, ib, iq, bank_data, fairway_data, erosion_inputs, pars, hfw, bcrds, velocity, bank_height, line_size,
+        water_level, chezy, dniqib, dviqib, dn_ship, dn_flow
+    ):
+        bcrds_mid = (bcrds[:-1] + bcrds[1:]) / 2
+
+        bank_coords_points = [Point(xy1) for xy1 in bcrds_mid]
+        bank_coords_geo = GeoSeries(bank_coords_points, crs=self.config_file.crs)
+        params = {
+            "chainage": bank_data.bank_chainage_midpoints[ib],
+            "x": bcrds_mid[:, 0],
+            "y": bcrds_mid[:, 1],
+            "iface_fw": bank_data.fairway_face_indices[ib],
+            "iface_bank": bank_data.bank_face_indices[ib],  # bank_index
+            "u": velocity[iq][ib],
+            "zb": bank_height[ib],
+            "len": line_size[ib],
+            "zw": water_level[iq][ib],
+            "zw0": fairway_data.fairway_initial_water_levels[ib],
+            "tauc": erosion_inputs.tauc[ib],
+            "nship": pars["n_ship"][ib],
+            "vship": pars["v_ship"][ib],
+            "nwave": pars["n_wave"][ib],
+            "shiptype": pars["ship_type"][ib],
+            "draught": pars["t_ship"][ib],
+            "mu_slp": pars["mu_slope"][ib],
+            "mu_reed": pars["mu_reed"][ib],
+            "dist_fw": bank_data.fairway_distances[ib],
+            "dfw0": erosion_inputs.wave_fairway_distance_0[ib],
+            "dfw1": erosion_inputs.wave_fairway_distance_1[ib],
+            "hfw": hfw,
+            "chez": chezy[iq][ib],
+            "zss": erosion_inputs.bank_protection_level[ib],
+            "dn": dniqib,
+            "dv": dviqib,
+            "dnship": dn_ship,
+            "dnflow": dn_flow,
+        }
+        write_shp(bank_coords_geo, params, f"{str(self.river_data.output_dir)}{os.sep}debug.Q{iq + 1}.B{ib + 1}.shp")
+        write_csv(params, f"{str(self.river_data.output_dir)}{os.sep}debug.Q{iq + 1}.B{ib + 1}.csv")
 
     def _prepare_initial_conditions(
         self, config_file: ConfigFile, num_stations_per_bank: List[int], fairway_data: FairwayData
@@ -598,42 +639,10 @@ class Erosion:
                 ship_wave_min[iq].append(ship_wave_min_ib)
 
                 if self.river_data.debug:
-                    bcrds_mid = (bcrds[:-1] + bcrds[1:]) / 2
-
-                    bank_coords_points = [Point(xy1) for xy1 in bcrds_mid]
-                    bank_coords_geo = GeoSeries(bank_coords_points, crs=config_file.crs)
-                    params = {
-                        "chainage": bank_data.bank_chainage_midpoints[ib],
-                        "x": bcrds_mid[:, 0],
-                        "y": bcrds_mid[:, 1],
-                        "iface_fw": bank_data.fairway_face_indices[ib],
-                        "iface_bank": bank_data.bank_face_indices[ib],  # bank_index
-                        "u": velocity[iq][ib],
-                        "zb": bank_height[ib],
-                        "len": line_size[ib],
-                        "zw": water_level[iq][ib],
-                        "zw0": fairway_data.fairway_initial_water_levels[ib],
-                        "tauc": erosion_inputs.tauc[ib],
-                        "nship": pars["n_ship"][ib],
-                        "vship": pars["v_ship"][ib],
-                        "nwave": pars["n_wave"][ib],
-                        "shiptype": pars["ship_type"][ib],
-                        "draught": pars["t_ship"][ib],
-                        "mu_slp": pars["mu_slope"][ib],
-                        "mu_reed": pars["mu_reed"][ib],
-                        "dist_fw": bank_data.fairway_distances[ib],
-                        "dfw0": erosion_inputs.wave_fairway_distance_0[ib],
-                        "dfw1": erosion_inputs.wave_fairway_distance_1[ib],
-                        "hfw": hfw,
-                        "chez": chezy[iq][ib],
-                        "zss": erosion_inputs.bank_protection_level[ib],
-                        "dn": dniqib,
-                        "dv": dviqib,
-                        "dnship": dn_ship,
-                        "dnflow": dn_flow,
-                    }
-                    write_shp(bank_coords_geo, params, f"{str(self.river_data.output_dir)}{os.sep}debug.Q{iq + 1}.B{ib + 1}.shp")
-                    write_csv(params, f"{str(self.river_data.output_dir)}{os.sep}debug.Q{iq + 1}.B{ib + 1}.csv")
+                    self.debug_process_discharge_levels_2(
+                        ib, iq, bank_data, fairway_data, erosion_inputs, pars, hfw, bcrds, velocity, bank_height, line_size,
+                        water_level, chezy, dniqib, dviqib, dn_ship, dn_flow
+                    )
 
                 # shift bank lines
                 if len(total_erosion_dist) == ib:
