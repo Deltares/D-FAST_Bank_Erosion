87fbe234e7f3118fd8233a33c3853c0c7d148e44
Mostafa Farrag <moah.farag@gmail.com>
2025-05-09 12:21:32 +0200

    reformat the kernel.py

--- src/dfastbe/kernel.py
+++ src/dfastbe/kernel.py
@@ -26,63 +26,67 @@ INFORMATION
 This file is part of D-FAST Bank Erosion: https://github.com/Deltares/D-FAST_Bank_Erosion
 """
 
-from typing import Tuple, List
-from dfastbe.bank_erosion.utils import get_km_bins
+from typing import List, Tuple
+
 import numpy as np
 
+from dfastbe.bank_erosion.utils import get_km_bins
 
-def get_zoom_extends(km_min: float, km_max: float, zoom_km_step: float, bank_crds: List[np.ndarray], bank_km: List[np.ndarray]) -> [List[Tuple[float, float]], List[Tuple[float, float, float, float]]]:
-    """
-    Zoom .
 
-    Arguments
-    ---------
-    km_min : float
-        Minimum value for the chainage range of interest.
-    km_max : float
-        Maximum value for the chainage range of interest.
-    zoom_km_step : float
-        Preferred chainage length of zoom box.
-    bank_crds : List[np.ndarray]
-        List of N x 2 np arrays of coordinates per bank.
-    bank_km : List[np.ndarray]
-        List of N np arrays of chainage values per bank.
+def get_zoom_extends(
+    km_min: float,
+    km_max: float,
+    zoom_km_step: float,
+    bank_crds: List[np.ndarray],
+    bank_km: List[np.ndarray],
+) -> tuple[list[tuple[float, float]], list[tuple[float, float, float, float]]]:
+    """Zoom.
 
-    Returns
-    -------
-    kmzoom : List[Tuple[float, float]]
-        Zoom ranges for plots with chainage along x-axis.
-    xyzoom : List[Tuple[float, float, float, float]]
-        Zoom ranges for xy-plots.
+    Args:
+        km_min (float):
+            Minimum value for the chainage range of interest.
+        km_max (float):
+            Maximum value for the chainage range of interest.
+        zoom_km_step (float):
+            Preferred chainage length of zoom box.
+        bank_crds (List[np.ndarray]):
+            List of N x 2 np arrays of coordinates per bank.
+        bank_km (List[np.ndarray]):
+            List of N np arrays of chainage values per bank.
+
+    Returns:
+        station_zoom (List[Tuple[float, float]]):
+            Zoom ranges for plots with chainage along x-axis.
+        coords_zoom (List[Tuple[float, float, float, float]]):
+            Zoom ranges for xy-plots.
     """
 
     zoom_km_bin = (km_min, km_max, zoom_km_step)
     zoom_km_bnd = get_km_bins(zoom_km_bin, station_type="all", adjust=True)
     eps = 0.1 * zoom_km_step
 
-    kmzoom: List[Tuple[float, float]] = []
-    xyzoom: List[Tuple[float, float, float, float]] = []
+    station_zoom: List[Tuple[float, float]] = []
+    coords_zoom: List[Tuple[float, float, float, float]] = []
     inf = float('inf')
-    for i in range(len(zoom_km_bnd)-1):
+    for i in range(len(zoom_km_bnd) - 1):
         km_min = zoom_km_bnd[i] - eps
         km_max = zoom_km_bnd[i + 1] + eps
-        kmzoom.append((km_min, km_max))
+        station_zoom.append((km_min, km_max))
 
-        xmin = inf
-        xmax = -inf
-        ymin = inf
-        ymax = -inf
+        x_min = inf
+        x_max = -inf
+        y_min = inf
+        y_max = -inf
         for ib in range(len(bank_km)):
-            irange = (bank_km[ib] >= km_min) & (bank_km[ib] <= km_max)
-            range_crds = bank_crds[ib][irange, :]
+            ind = (bank_km[ib] >= km_min) & (bank_km[ib] <= km_max)
+            range_crds = bank_crds[ib][ind, :]
             x = range_crds[:, 0]
             y = range_crds[:, 1]
             if len(x) > 0:
-                xmin = min(xmin, min(x))
-                xmax = max(xmax, max(x))
-                ymin = min(ymin, min(y))
-                ymax = max(ymax, max(y))
-        xyzoom.append((xmin, xmax, ymin, ymax))
+                x_min = min(x_min, min(x))
+                x_max = max(x_max, max(x))
+                y_min = min(y_min, min(y))
+                y_max = max(y_max, max(y))
+        coords_zoom.append((x_min, x_max, y_min, y_max))
 
-    return kmzoom, xyzoom
-
+    return station_zoom, coords_zoom
