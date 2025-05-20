615d5fecd91969fb0b78cead8ec4dd8946da48cb
Mostafa Farrag <moah.farag@gmail.com>
2025-05-20 13:18:23 +0200

    test

--- tests/test_erosion/test_bank_erosion.py
+++ tests/test_erosion/test_bank_erosion.py
@@ -16,7 +16,7 @@ def test_bank_erosion():
     file = "erosion"
     language = "UK"
     config_file = f"tests/data/{file}/meuse_manual.cfg"
-    run(language, "BANKLINES", config_file)
+     # run(language, "BANKLINES", config_file)
     print("Banklines done")
     run(language, "BANKEROSION", config_file)
     print("Bank erosion done")
