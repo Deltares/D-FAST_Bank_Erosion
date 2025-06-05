8c4b445f7d74b07d95c1499a93ebf1eb7b05f953
Jeroen <jeroen.noort@deltares.nl>
2025-06-05 10:23:04 +0200

    add a test for `compute_bank_erosion_dynamics`

--- tests/test_erosion/test_erosion_calculator.py
+++ tests/test_erosion/test_erosion_calculator.py
@@ -38,6 +38,7 @@ class TestErosionCalculator:
         erosion_inputs.wave_fairway_distance_0 = np.array([150.0, 150.0, 150.0])
         erosion_inputs.wave_fairway_distance_1 = np.array([110.0, 110.0, 110.0])
         erosion_inputs.bank_protection_level = np.array([-13.0, -13.0, -13.0])
+        erosion_inputs.tauc = np.array([1.0, 1.0, 1.0])
         return erosion_inputs
 
     @pytest.fixture
@@ -78,4 +79,41 @@ class TestErosionCalculator:
         )
         assert np.allclose(
             eq_erosion_volume, np.array([78.0826197, 328.12862225, 421.77232935])
+        )
+
+    def test_compute_bank_erosion_dynamics(
+        self, single_calculation, single_parameters, erosion_inputs, shared_input
+    ):
+        single_calculation = ErosionCalculator.compute_bank_erosion_dynamics(
+            single_calculation=single_calculation,
+            bank_height=shared_input["bank_height"],
+            segment_length=shared_input["segment_length"],
+            bank_fairway_dist=shared_input["bank_fairway_dist"],
+            water_level_fairway_ref=shared_input["water_level_fairway_ref"],
+            discharge_level_pars=single_parameters,
+            time_erosion=1.0,
+            erosion_inputs=erosion_inputs,
+        )
+
+        assert np.allclose(
+            single_calculation.erosion_volume_tot,
+            np.array([0.0, 0.0, 0.0]),
+        )
+        assert np.allclose(
+            single_calculation.erosion_distance_tot,
+            np.array([0.0, 0.0, 0.0]),
+        )
+        assert np.allclose(
+            single_calculation.erosion_distance_shipping, np.array([0.0, 0.0, 0.0])
+        )
+        assert np.allclose(
+            single_calculation.erosion_distance_flow, np.array([0.0, 0.0, 0.0])
+        )
+        assert np.allclose(
+            single_calculation.ship_wave_max,
+            np.array([1.1096215, 1.10866935, 1.10853603]),
+        )
+        assert np.allclose(
+            single_calculation.ship_wave_min,
+            np.array([0.56151398, 0.5653226, 0.56585589]),
         )
