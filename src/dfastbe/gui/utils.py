6a5a2a77fdf007838ea5b9e4caaefc19dfe94d9a
Mostafa Farrag <moah.farag@gmail.com>
2026-01-29 12:56:22 +0100

    move utils functionalities to their calling modules and update imports

--- src/dfastbe/gui/utils.py
+++ src/dfastbe/gui/utils.py
@@ -12,7 +12,6 @@ from PySide6.QtWidgets import (
     QGridLayout,
     QWidget,
     QFileDialog,
-    QTreeWidgetItem,
     QSizePolicy,
     QSpacerItem,
     QComboBox
@@ -41,11 +40,11 @@ __all__ = [
     "edit_search_line",
     "addOpenFileRow",
     "typeUpdatePar",
+    "selectFile",
     "addTabForLevel",
-    "add_an_item",
-    "remove_an_item",
-    "editADischarge",
-    "generalParLayout"
+    "generalParLayout",
+    "set_dialog_size",
+    "close_edit"
 ]
 
 
@@ -417,76 +416,6 @@ def selectFile(key: str) -> None:
     if fil != "":
         state_management[key].setText(fil)
 
-
-def add_an_item(key: str) -> None:
-    """Implements the actions for the add item button.
-
-    Args:
-        key : str
-            Short name of the parameter.
-    """
-    state_management = StateStore.instance()
-    n_items = state_management[key].invisibleRootItem().childCount()
-    i = n_items + 1
-    istr = str(i)
-    if key == "searchLines":
-        file_name, dist = edit_search_line(key, istr)
-        c1 = QTreeWidgetItem(state_management["searchLines"], [istr, file_name, dist])
-    elif key == "discharges":
-        prob = str(1 / (n_items + 1))
-        file_name, prob = editADischarge(key, istr, prob=prob)
-        c1 = QTreeWidgetItem(state_management["discharges"], [istr, file_name, prob])
-        addTabForLevel(istr)
-        state_management["refLevel"].validator().setTop(i)
-
-    state_management[key + "Edit"].setEnabled(True)
-    state_management[key + "Remove"].setEnabled(True)
-
-
-def editADischarge(key: str, istr: str, file_name: str = "", prob: str = ""):
-    """Create an edit dialog for simulation file and weighing.
-
-    Args:
-        key : str
-            Short name of the parameter.
-        istr : str
-            String representation of the simulation in the list.
-        file_name : str
-            Name of the simulation file.
-        prob : str
-            String representation of the weight for this simulation.
-    """
-    state_management = StateStore.instance()
-    edit_dialog = QDialog()
-    set_dialog_size(edit_dialog, 600, 100)
-    edit_dialog.setWindowFlags(
-        Qt.WindowTitleHint | Qt.WindowSystemMenuHint
-    )
-    edit_dialog.setWindowTitle("Edit Discharge")
-    edit_layout = QFormLayout(edit_dialog)
-
-    label = QLabel(istr)
-    edit_layout.addRow("Level Nr", label)
-
-    addOpenFileRow(edit_layout, "editDischarge", "Simulation File")
-    state_management["editDischargeEdit"].setText(file_name)
-
-    probability = QLineEdit()
-    probability.setText(prob)
-    probability.setValidator(validator("positive_real"))
-    edit_layout.addRow("Probability [-]", probability)
-
-    done = QPushButton("Done")
-    done.clicked.connect(lambda: close_edit(edit_dialog))
-
-    edit_layout.addRow(" ", done)
-
-    edit_dialog.exec()
-
-    file_name = state_management["editDischargeEdit"].text()
-    prob = probability.text()
-    return file_name, prob
-
 def addTabForLevel(istr: str) -> None:
     """Create the tab for the settings associated with simulation i.
 
@@ -601,69 +530,6 @@ def typeUpdatePar(key: str) -> None:
         state_management[key + "Edit"].setEnabled(True)
         state_management[key + "Edit"].setValidator(None)
         state_management[key + "EditFile"].setEnabled(True)
-
-
-def remove_an_item(key: str) -> None:
-    """Implements the actions for the remove item button.
-
-    Args:
-        key : str
-            Short name of the parameter.
-    """
-    state_management = StateStore.instance()
-    selected = state_management[key].selectedItems()
-    root = state_management[key].invisibleRootItem()
-    if len(selected) > 0:
-        istr = selected[0].text(0)
-        root.removeChild(selected[0])
-        i = int(istr) - 1
-        for j in range(i, root.childCount()):
-            root.child(j).setText(0, str(j + 1))
-    else:
-        istr = ""
-    if root.childCount() == 0:
-        state_management[key + "Edit"].setEnabled(False)
-        state_management[key + "Remove"].setEnabled(False)
-    if istr == "":
-        pass
-    elif key == "searchLines":
-        pass
-    elif key == "discharges":
-        tabs = state_management["tabs"]
-        state_management["refLevel"].validator().setTop(root.childCount())
-        dj = 0
-        for j in range(tabs.count()):
-            if dj > 0:
-                tabs.setTabText(j - 1, "Level " + str(j + dj))
-                update_tab_keys(j + dj + 1)
-            elif tabs.tabText(j) == "Level " + istr:
-                tabs.removeTab(j)
-                dj = i - j
-
-
-def update_tab_keys(i: int) -> None:
-    """Renumber tab i to tab i-1.
-
-    Args:
-        i : str
-            Number of the tab to be updated.
-    """
-    state_management = StateStore.instance()
-    iStart = str(i) + "_"
-    newStart = str(i - 1) + "_"
-    N = len(iStart)
-    keys = [key for key in state_management.keys() if key[:N] == iStart]
-    for key in keys:
-        obj = state_management.pop(key)
-        if key[-4:] == "Type":
-            obj.currentIndexChanged.disconnect()
-            obj.currentIndexChanged.connect(
-                lambda: typeUpdatePar(newStart + key[N:-4])
-            )
-        elif key[-4:] == "File":
-            obj.clicked.disconnect()
-            obj.clicked.connect(lambda: selectFile(newStart + key[N:-4]))
-        state_management[newStart + key[N:]] = obj
 
 
 def generalParLayout(
